a10143b5fad04e761da7df2abb56e4cdc24d72e2
Socea Tiberiu <soceatiberiuv@gmail.com>
2019-03-18 18:59:54 +0200

    Implemented APIs for customers and Movies

---
+++ VideoRental/App_Start/MappingProfile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using VideoRental.Dtos;
+using VideoRental.Models;
+
+namespace VideoRental.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //CUSTOMER MAPPING
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+            Mapper.CreateMap<Customer, CustomerDto>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+
+            //MOVIE MAPPING
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MovieDto, Movie>();
+            Mapper.CreateMap<Movie, MovieDto>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+
+        }
+    }
+}
